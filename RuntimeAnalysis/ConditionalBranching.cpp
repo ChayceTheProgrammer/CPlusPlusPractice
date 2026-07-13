dab9d061d5080e5f75b5a4108b370f2c78354691
Chayce <104463214+ChayceTheProgrammer@users.noreply.github.com>
2026-07-12 22:12:09 -0500

    Conditional Runtime Analysis

--- RuntimeAnalysis/ConditionalBranching.cpp
+++ RuntimeAnalysis/ConditionalBranching.cpp
@@ -1,7 +1,29 @@
 #include<iostream>
 using namespace std;
 
+/*
+The Runtime of an if-else statement (conditional) is bound by the runtime of the conditional test + 
+    larger of the runtimes of the executions
+
+Runtime Calculation:
+    O(condition) + max(O(s1), O(s2))
+*/
+
+
 int main(){
+
+
+    if (true)
+    {
+        //Block 1
+    } 
+    else
+    {
+        //Block 2
+    }
+    
+    
+
 
     return 0;
 }
